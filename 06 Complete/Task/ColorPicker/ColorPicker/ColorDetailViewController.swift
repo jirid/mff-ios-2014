1c6a58ab295e086105d9c604553aa9db54a3296a
Jiri Dutkevic <jiri@dutkevic.com>
2014-12-11 21:38:17 +0100

    * implemented image download

--- 06 Complete/Task/ColorPicker/ColorPicker/ColorDetailViewController.swift
+++ 06 Complete/Task/ColorPicker/ColorPicker/ColorDetailViewController.swift
@@ -36,8 +36,34 @@ class ColorDetailViewController: UIViewController {
                 redLabel.text = "\(c.red)"
                 greenLabel.text = "\(c.green)"
                 blueLabel.text = "\(c.blue)"
-                imageView.image = c.image
+                displayImage(c)
             }
         }
+    }
+    
+    func displayImage(c: Color)
+    {
+        if let img = c.image {
+            imageView.image = img
+        } else if let url = c.imageUrl {
+            downloadImage(url, c)
+        } else {
+            imageView.image = nil
+        }
+    }
+    
+    func downloadImage(url: NSURL, _ c: Color)
+    {
+        let session = NSURLSession(configuration: NSURLSessionConfiguration.ephemeralSessionConfiguration())
+        let task = session.downloadTaskWithURL(url, completionHandler: { [weak self] (url2, response, error) -> () in
+            c.setImage(UIImage(data: NSData(contentsOfURL: url2)!)!)
+            dispatch_async(dispatch_get_main_queue())
+            { //[weak self] in
+                let j = 0
+                self?.imageView.image = c.image
+                let i = 0
+            }
+        })
+        task.resume()
     }
 }
