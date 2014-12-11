e3caad65dbaeb03a46820f5cc0c1f36d0ee5ec61
Jiri Dutkevic <jiri@dutkevic.com>
2014-12-11 20:22:53 +0100

    * navigation solved

---
+++ 04 Table/Task/ColorPicker/ColorPicker/MasterViewController.swift
@@ -0,0 +1,46 @@
+//
+//  MasterViewController.swift
+//  ColorPicker
+//
+//  Created by Jiri Dutkevic on 11/12/14.
+//
+//
+
+import UIKit
+
+class MasterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController!.navigationBar.barTintColor = UIColor.grayColor()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction
+    func colorSelected(segue: UIStoryboardSegue) {
+        let detail = segue.sourceViewController as ColorDetailViewController
+        let color = detail.color
+        let r = CGFloat(color.red) / 255.0
+        let g = CGFloat(color.green) / 255.0
+        let b = CGFloat(color.blue) / 255.0
+        let c = UIColor(red: r, green: g, blue: b, alpha: 0.9);
+        self.navigationController!.navigationBar.barTintColor = c
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
