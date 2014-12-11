50b0d792978eaabd47d8c3cbb6f482ff367ab30f
Jiri Dutkevic <jiri@dutkevic.com>
2014-12-11 19:47:01 +0100

    * iphone layout

--- 03 Containers/Task/ColorPicker/ColorPicker/ColorDetailViewController.swift
+++ 03 Containers/Task/ColorPicker/ColorPicker/ColorDetailViewController.swift
@@ -9,27 +9,22 @@
 import UIKit
 
 class ColorDetailViewController: UIViewController {
+    
+    var color = DataSource.oneColor()
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        titleLabel.text = color.name
+        redLabel.text = "\(color.red)"
+        greenLabel.text = "\(color.green)"
+        blueLabel.text = "\(color.blue)"
+        imageView.image = color.image
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
